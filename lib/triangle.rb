c3246b6a16cf97edbe1671240200cad5a35bd4b4
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-18 16:58:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
   # write code here
-end
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    if @side_1 == @side_2 && @side_1 == @side_3
+      return :equilateral
+    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+  class TriangleError < StandardError
+
+  end
