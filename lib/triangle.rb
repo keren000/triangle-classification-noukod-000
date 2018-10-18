58a6f0243c579ff9f2e4b423dc1e0794597b2c7d
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-18 17:14:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
   # write code here\
 
-  def initialize(s1, s2, s3)  
+  def initialize(s1, s2, s3)
     if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
       raise TriangleError
     end
