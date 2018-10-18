705744e9e5c0a1a115bd28b9b54ca71371093f07
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-18 17:21:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,8 +6,7 @@ class Triangle
     @side3 = side3
   end
    def kind
-    if (@side1 == @side2 && @side2 == @side3 && @side1 == 0) || (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1) || (@side1 + @side3 <= @side2)
-      raise TriangleError
+    
     elsif @side1 == @side2 && @side2 == @side3
       :equilateral
     elsif @side2 == @side3 || @side1 == @side3 || @side1 == @side2
