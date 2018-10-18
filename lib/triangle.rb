9238a772f9bc34be4a99ebb8a7c9ff4be14b7ec0
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-18 17:21:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,36 +1,21 @@
 class Triangle
-  attr_accessor :side_1, :side_2, :side_3
-  # write code here\
-
-  def initialize(side_1, side_2, side_3)
-    if side_1 >= (side_2 + side_3) || side_2 >= (side_1 + side_3) || side_3 >= (side_1 + side_2)
-      raise TriangleError
-    end
-
-    if side_1 <= 0 || side_2 <= 0 ||side_3 <= 0
-      raise TriangleError
-    end
-
-    if side_1 == nil || side_2 == nil ||side_3 == nil
-      raise TriangleError
-    end
-
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+  # write code here
+   def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
-
-  def kind
-    if @side_1 == @side_2 && @side_1 == @side_3
-      return :equilateral
-    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
-      return :isosceles
+   def kind
+    if (@side1 == @side2 && @side2 == @side3 && @side1 == 0) || (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1) || (@side1 + @side3 <= @side2)
+      raise TriangleError
+    elsif @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side2 == @side3 || @side1 == @side3 || @side1 == @side2
+      :isosceles
     else
-      return :scalene
+      :scalene
     end
   end
 end
-
-class TriangleError < StandardError
-
-end
+ class TriangleError < StandardError
+ end
