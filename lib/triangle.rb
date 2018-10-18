c2b2cdede8396d6bcef43764445da6e344d534ae
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-18 17:26:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,11 +10,11 @@ class Triangle
     if (@side1 == @side2 && @side2 == @side3 && @side1 == 0) || (@side1 + @side2 <= @side3) || (@side2 + @side3 <= @side1) || (@side1 + @side3 <= @side2)
       raise TriangleError
     elsif @side1 == @side2 && @side2 == @side3
-      return equilateral
+      :equilateral
     elsif @side2 == @side3 || @side1 == @side3 || @side1 == @side2
-      return isosceles
+      :isosceles
     else
-      return scalene
+      :scalene
     end
   end
 end
