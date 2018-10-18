c1832d19ad92754c4cf1d5fe4a947d4536d75324
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-18 17:16:28 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,29 +1,29 @@
 class Triangle
-  attr_accessor :side_1, 
+  attr_accessor :side_1,
   # write code here\
 
-  def initialize(s1, s2, s3)
-    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
+  def initialize(side_1, side_2, side_3)
+    if side_1 >= (side_2 + side_3) || side_2 >= (side_1 + side_3) || side_3 >= (side_1 + side_2)
       raise TriangleError
     end
 
-    if s1 <= 0 || s2 <= 0 ||s3 <= 0
+    if side_1 <= 0 || side_2 <= 0 ||side_3 <= 0
       raise TriangleError
     end
 
-    if s1 == nil || s2 == nil ||s3 == nil
+    if side_1 == nil || side_2 == nil ||side_3 == nil
       raise TriangleError
     end
 
-    @s1 = s1
-    @s2 = s2
-    @s3 = s3
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
 
   def kind
-    if @s1 == @s2 && @s1 == @s3
+    if @side_1 == @side_2 && @side_1 == @side_3
       return :equilateral
-    elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1
+    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
       return :isosceles
     else
       return :scalene
